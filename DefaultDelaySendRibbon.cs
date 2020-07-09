cba1a33e6e450d1b48bbc10f6b417ad840a51075
Matt Miller <mmiller@hick.org>
2020-07-09 12:43:55 -0700

    initial repo commit

---
+++ DefaultDelaySendRibbon.cs
@@ -0,0 +1,228 @@
+﻿using DefaultDelaySend.Properties;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Tools.Outlook;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace DefaultDelaySend
+{
+    [ComVisible(true)]
+    public class DefaultDelaySendRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI explorerRibbon;
+        private Office.IRibbonUI mailRibbon;
+
+        public DefaultDelaySendRibbon()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            string ribbonXML = String.Empty;
+
+            if (ribbonID == "Microsoft.Outlook.Mail.Compose")
+            {
+                ribbonXML = GetResourceText("DefaultDelaySend.MailTab.xml");
+            }
+            else if (ribbonID == "Microsoft.Outlook.Explorer")
+            {
+                ribbonXML = GetResourceText("DefaultDelaySend.ExplorerTab.xml");
+            }
+
+            return ribbonXML;
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+
+        public void ExplorerRibbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.explorerRibbon = ribbonUI;
+        }
+
+        public void MailRibbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.mailRibbon = ribbonUI;
+        }
+
+        public void outOfOfficeDayCheckBox_onAction(Office.IRibbonControl control, bool isPressed)
+        {
+            if (control.Id.StartsWith("DelaySend_OutOfOffice"))
+            {
+                string settingName = control.Id.Replace("DelaySend_", "");
+
+                Type defaultType = typeof(DefaultDelaySendSettings);
+                PropertyInfo propertyInfo = defaultType.GetProperty(settingName);
+
+                if (propertyInfo != null)
+                {
+                    propertyInfo.SetValue(DefaultDelaySendSettings.Default, isPressed);
+                    DefaultDelaySendSettings.Default.Save();
+                    ThisAddIn.RefreshSettings();
+                }
+            }
+        }
+
+        public bool outOfOfficeDayCheckBox_getPressed(Office.IRibbonControl control)
+        {
+            if (control.Id.StartsWith("DelaySend_OutOfOffice"))
+            {
+                string settingName = control.Id.Replace("DelaySend_", "");
+
+                Type defaultType = typeof(DefaultDelaySendSettings);
+                PropertyInfo propertyInfo = defaultType.GetProperty(settingName);
+
+                if (propertyInfo != null)
+                {
+                    return (bool)propertyInfo.GetValue(DefaultDelaySendSettings.Default);
+                }
+            }
+
+            return false;
+        }
+
+        public void businessHourEditBox_onChange(Office.IRibbonControl control, string text)
+        {
+            if (control.Id.StartsWith("DelaySend_"))
+            {
+                string settingName = control.Id.Replace("DelaySend_", "");
+                int value;
+
+                if (settingName == "StartBusinessHour")
+                {
+                    if (Int32.TryParse(text, out value))
+                    {
+                        DefaultDelaySendSettings.Default.StartBusinessHour = value;
+                    }
+                }
+                else if (settingName == "EndBusinessHour")
+                {
+                    if (Int32.TryParse(text, out value))
+                    {
+                        DefaultDelaySendSettings.Default.EndBusinessHour = value;
+                    }
+                }
+
+                DefaultDelaySendSettings.Default.Save();
+                ThisAddIn.RefreshSettings();
+            }
+        }
+
+        public string businessHourEditBox_getText(Office.IRibbonControl control)
+        {
+            if (control.Id.StartsWith("DelaySend_"))
+            {
+                string settingName = control.Id.Replace("DelaySend_", "");
+
+                if (settingName == "StartBusinessHour")
+                {
+                    return DefaultDelaySendSettings.Default.StartBusinessHour.ToString();
+                }
+                else if (settingName == "EndBusinessHour")
+                {
+                    return DefaultDelaySendSettings.Default.EndBusinessHour.ToString();
+                }
+            }
+
+            return "";
+        }
+
+        public void overrideDelaySendCheckBox_onAction(Office.IRibbonControl control, bool isPressed)
+        {
+            object context = control.Context;
+            Func<Outlook.MailItem, bool> evaluateDelaySendOverrideLambda = (Outlook.MailItem mailItem) =>
+            {
+                UserProperty prop = mailItem.UserProperties.Find("OverrideDelaySend");
+
+                if (prop == null)
+                {
+                    prop = mailItem.UserProperties.Add("OverrideDelaySend", OlUserPropertyType.olYesNo);
+                }
+
+                if (isPressed)
+                {
+                    prop.Value = true;
+                }
+                else
+                {
+                    prop.Value = false;
+                }
+
+                return true;
+            };
+
+            if (context is Outlook.Explorer)
+            {
+                var explorer = context as Outlook.Explorer;
+
+                Outlook.Selection selection = null;
+                try
+                {
+                    selection = explorer.Selection;
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+                if (selection != null)
+                {
+                    foreach (object item in selection)
+                    {
+                        if (item is Outlook.MailItem)
+                        {
+                            evaluateDelaySendOverrideLambda(item as Outlook.MailItem);
+                        }
+                    }
+                }
+            }
+            else if (context is Outlook.Inspector)
+            {
+                var inspector = context as Outlook.Inspector;
+                object item = inspector.CurrentItem;
+               
+                if (item is Outlook.MailItem)
+                {
+                    evaluateDelaySendOverrideLambda(item as Outlook.MailItem);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Core;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+using DefaultDelaySend.Properties;
+
+namespace DefaultDelaySend
+{
+    public partial class ThisAddIn
+    {
+        public static HashSet<DayOfWeek> OutOfOfficeDays = new HashSet<DayOfWeek>();
+
+        public static void RefreshSettings()
+        {
+            var days = new (DayOfWeek, bool)[]
+            {
+                (DayOfWeek.Monday, DefaultDelaySendSettings.Default.OutOfOfficeMonday),
+                (DayOfWeek.Tuesday, DefaultDelaySendSettings.Default.OutOfOfficeTuesday),
+                (DayOfWeek.Wednesday, DefaultDelaySendSettings.Default.OutOfOfficeWednesday),
+                (DayOfWeek.Thursday, DefaultDelaySendSettings.Default.OutOfOfficeThursday),
+                (DayOfWeek.Friday, DefaultDelaySendSettings.Default.OutOfOfficeFriday),
+                (DayOfWeek.Saturday, DefaultDelaySendSettings.Default.OutOfOfficeSaturday),
+                (DayOfWeek.Sunday, DefaultDelaySendSettings.Default.OutOfOfficeSunday)
+            };
+
+            OutOfOfficeDays.Clear();
+
+            foreach (var day in days)
+            {
+                if (day.Item2)
+                {
+                    OutOfOfficeDays.Add(day.Item1);
+                }
+            }
+        }
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            RefreshSettings();
+
+            Application.ItemSend += Application_ItemSend;
+        }
+
+        private void Application_ItemSend(object item, ref bool Cancel)
+        {
+            // Only apply delay delivery to mail items.
+            if (item is Outlook.MailItem == false)
+            {
+                return;
+            }
+
+            var mailItem = item as Outlook.MailItem;
+
+            var sendDate = DateTime.Now;
+            var delayDate = DateTime.Now;
+            bool outsideBusinessHours = false;
+
+            // Check to see if this mail is being sent outside of business hours.
+            if (OutOfOfficeDays.Contains(sendDate.DayOfWeek) ||
+                (sendDate.Hour > DefaultDelaySendSettings.Default.EndBusinessHour))
+            {
+                // This mail was sent on a non-business day or after business hours on a business day, so
+                // we need to delay to at least the next day (and possibly longer).
+                outsideBusinessHours = true;
+
+                do
+                {
+                    delayDate = delayDate.AddDays(1);
+
+                    // If all of the business days are out of office, then return so that we don't
+                    // loop forever.
+                    if (delayDate.DayOfWeek == sendDate.DayOfWeek)
+                    {
+                        return;
+                    }
+
+                } while (OutOfOfficeDays.Contains(delayDate.DayOfWeek));
+
+                delayDate = new DateTime(delayDate.Year, delayDate.Month, delayDate.Day, DefaultDelaySendSettings.Default.StartBusinessHour, 0, 0);
+            }
+            else if (sendDate.Hour <= DefaultDelaySendSettings.Default.StartBusinessHour)
+            {
+                // This mail was sent before business hours on the current day, so delay send to the start hour.
+                outsideBusinessHours = true;
+
+                delayDate = new DateTime(delayDate.Year, delayDate.Month, delayDate.Day, DefaultDelaySendSettings.Default.StartBusinessHour, 0, 0);
+            }
+
+            if (outsideBusinessHours == false)
+            {
+                return;
+            }
+
+            // Check to see if we should override delay send.
+            UserProperty overrideDelaySendProp = mailItem.UserProperties.Find("OverrideDelaySend");
+            bool overrideDelaySend = (overrideDelaySendProp != null && overrideDelaySendProp.Value == true);
+
+            if (overrideDelaySend == true)
+            {
+                mailItem.DeferredDeliveryTime = sendDate;
+                return;
+            }
+
+            // At this point, the mail has been sent outside of business hours and
+            // the user did not elect to override delay send. Delay delivery to the
+            // next business day.
+
+            var prop = mailItem.UserProperties.Add("DelaySend", OlUserPropertyType.olYesNo);
+            prop.Value = true;
+
+            mailItem.DeferredDeliveryTime = delayDate;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new DefaultDelaySendRibbon();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
